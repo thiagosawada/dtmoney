4c503504ed1f738c9d28ffa40c75e1490e929e40
Thiago <tms1_tms2@hotmail.com>
2021-08-22 10:49:24 -0300

    Refactor

--- src/TransactionsContext.tsx
+++ src/TransactionsContext.tsx
@@ -1,8 +1,6 @@
 import { createContext, ReactNode, useEffect, useState } from "react";
 import { api } from "./services/api";
 
-export const TransactionsContext = createContext<Transaction[]>([]);
-
 interface Transaction {
   id: number;
   title: string;
@@ -12,9 +10,20 @@ interface Transaction {
   value: number;
 }
 
+type TransactionInput = Omit<Transaction, "id" | "createdAt">;
+
 interface TransactionsProviderProps {
   children: ReactNode;
 }
+
+interface TransactionsContextData {
+  transactions: Transaction[];
+  createTransaction: (transaction: TransactionInput) => void;
+}
+
+export const TransactionsContext = createContext<TransactionsContextData>(
+  {} as TransactionsContextData // forçar tipagem
+);
 
 export function TransactionsProvider({ children }: TransactionsProviderProps) {
   const [transactions, setTransactions] = useState<Transaction[]>([]);
@@ -24,8 +33,15 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
       .then(response => setTransactions(response.data.transactions));
   }, [])
 
+  function createTransaction(transaction: TransactionInput) {
+    api.post("/transactions", transaction);
+  }
+
   return (
-    <TransactionsContext.Provider value={transactions}>
+    <TransactionsContext.Provider value={{
+      transactions,
+      createTransaction
+    }}>
       { children }
     </TransactionsContext.Provider>
   );
--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,9 +1,10 @@
-import { FormEvent, useState } from "react";
+import { FormEvent, useState, useContext } from "react";
 import Modal from "react-modal";
+import { TransactionsContext } from "../../TransactionsContext";
+
 import closeImg from "../../assets/close.svg"
 import incomeImg from "../../assets/income.svg"
 import outcomeImg from "../../assets/outcome.svg"
-import { api } from "../../services/api";
 
 import { Container, RadioBox, TransactionTypeContainer } from "./styles";
 
@@ -15,6 +16,8 @@ interface NewTransactionModalProps {
 export function NewTransactionModal({
   isOpen, onRequestClose
 }: NewTransactionModalProps) {
+  const { createTransaction } = useContext(TransactionsContext)
+
   const [title, setTitle] = useState("");
   const [value, setValue] = useState(0);
   const [category, setCategory] = useState("");
@@ -22,8 +25,7 @@ export function NewTransactionModal({
 
   function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
-    const data = { title, value, category, type, };
-    api.post("/transactions", data);
+    createTransaction({ title, value, category, type })
   }
 
   return (
